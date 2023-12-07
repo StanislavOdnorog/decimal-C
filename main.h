3e9845447f609531d882bb1ea7f83c3674b49688
Saaaas <124214595+StanislavOdnorog@users.noreply.github.com>
2023-12-07 14:31:37 +0300

    Update main.h

--- main.h
+++ main.h
@@ -9,46 +9,43 @@
 #include <math.h>
 
 // MACRO //
-#define SIGN_EXP 7
 #define SIGN_HEX_POS 0x80
 #define SIGN_HEX_NEG 0
-#define MAX_EXP_NUM 128
-#define MAX_10_EXP 29
+#define SIGN_EXP_BITE_POS 7
 
-#define MAX_BITS 127
+#define MAX_EXP 128
+#define MAX_DECIMAL_EXP 29
+#define MAX_UNSIGNED 0xFFFFFFFF
+
+#define MANTIS_1_PART 32
+#define MANTIS_END 127
 #define PART_BITS 32
-#define MAX_UNS 0xFFFFFFFF
-
-#define M1 32
-#define M2 64
-#define M3 96
 
 #define ZERO {SIGN_HEX_POS, 0, 0, 0}
 #define ONE {SIGN_HEX_POS, 1, 0, 0}
 #define FIVE {SIGN_HEX_POS, 5, 0, 0}
 #define TEN {SIGN_HEX_POS, 10, 0, 0}
-#define MAX {SIGN_HEX_POS,MAX_UNS,MAX_UNS,0xFFFFFFF}
+#define MAX {SIGN_HEX_POS,MAX_UNSIGNED,MAX_UNSIGNED,0xFFFFFFF}
 
 // STRUCTURE //
 typedef struct{
-  unsigned e;
-  unsigned m[3];
+  unsigned data;
+  unsigned mantis[3];
 } s21_decimal;
 
 // BIT MANIPULATION //
-void set_bit(s21_decimal *num, unsigned n, unsigned val);
-unsigned get_bit(s21_decimal num, unsigned n);
-void print_dec_bin(s21_decimal num);
-bool get_sign(s21_decimal num);
-void set_sign(s21_decimal *num, bool sign);
-s21_decimal s21_shift_l(s21_decimal a, int n);
-s21_decimal s21_shift_r(s21_decimal a, int n);
+s21_decimal shift_decimal_left(s21_decimal decimal, int shift);
+s21_decimal shift_decimal_right(s21_decimal decimal, int shift);
 
-// DATA UNSIGNED GETTERS AND SETTERS //
-unsigned get_exp(s21_decimal num);
+// DATA GETTERS AND SETTERS //
+void set_bit(s21_decimal *decimal, unsigned position, bool value);
+bool get_bit(s21_decimal decimal, unsigned position);
+bool get_sign(s21_decimal decimal);
+void set_sign(s21_decimal *decimal, bool sign);
+unsigned get_exp(s21_decimal decimal);
 void set_exp(s21_decimal *num, unsigned n);
-void add_exp(s21_decimal *num, int n);
-void sub_exp(s21_decimal *num, int n);
+void add_exp(s21_decimal *decimal, unsigned value);
+void sub_exp(s21_decimal *decimal, unsigned value);
 
 // COMPARATORS //
 int s21_is_zero(s21_decimal a);
@@ -62,9 +59,9 @@ int s21_is_less(s21_decimal a, s21_decimal b);
 int s21_is_less_or_equal(s21_decimal a, s21_decimal b);
 
 // ARITHMETICS //
-s21_decimal add_fract(s21_decimal a, s21_decimal b);
-s21_decimal sub_fract(s21_decimal a, s21_decimal b);
 void normalize_decs(s21_decimal *a, s21_decimal *b);
+s21_decimal add_helper(s21_decimal a, s21_decimal b);
+s21_decimal sub_helper(s21_decimal a, s21_decimal b);
 void div_helper(
 		s21_decimal *a,
 		s21_decimal *b,
@@ -76,12 +73,13 @@ void div_helper(
 		s21_decimal *res,
 		s21_decimal *mult);
 
-s21_decimal s21_add(s21_decimal a, s21_decimal b);
-s21_decimal s21_sub(s21_decimal a, s21_decimal b);
-s21_decimal s21_mul(s21_decimal a, s21_decimal b);
-s21_decimal s21_div(s21_decimal a, s21_decimal b);
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
+int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
+int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
+int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 
-// PRINT DECIMAL (WAY TO HEAVY!!!) //
+// PRINT DECIMAL // 
+void print_decimal_binary(s21_decimal decimal);
 void print_list(char* list, char exp);
 char *form_list_from_num(char *list, char num, char zeroes);
 char *add_list_to_list(char *a, char *b);
@@ -104,8 +102,14 @@ int s21_negate(s21_decimal value, s21_decimal *result);
 // TESTS //
 void add_test(unsigned sign1, unsigned sign2, unsigned num1, unsigned num2, unsigned exp1, unsigned exp2);
 void mul_test(unsigned sign1, unsigned sign2, unsigned num1, unsigned num2, unsigned exp1, unsigned exp2);
-void sub_test(unsigned sign1, unsigned sign2, unsigned num1, unsigned num2, unsigned exp1, unsigned exp2);
 void div_test(unsigned sign1, unsigned sign2, unsigned num1, unsigned num2, unsigned exp1, unsigned exp2);
-
+void sub_test(unsigned sign1, unsigned sign2, unsigned num1, unsigned num2, unsigned exp1, unsigned exp2);
+void from_dec_to_int_test(unsigned sign1, unsigned num1, unsigned exp1);
+void from_int_to_dec_test(int value);
+void from_float_to_dec_test(float value);
+void from_dec_to_float_test(unsigned sign1, unsigned num1, unsigned exp1);
+void truncate_test(unsigned sign1, unsigned num1, unsigned exp1);
+void round_test(unsigned sign1, unsigned num1, unsigned exp1);
+void floor_test(unsigned sign1, unsigned num1, unsigned exp1);
 
 #endif
